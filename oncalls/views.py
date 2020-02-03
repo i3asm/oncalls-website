17939811bb811a32f56e9cec6aec82a561c0050c
i3asm <i3asmmail@gmail.com>
2020-02-03 18:58:55 +0300

    second commit

--- oncalls/views.py
+++ oncalls/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+
 from django.http import HttpResponse
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def home(request):
+    return HttpResponse("it's working !!")
